03d3b144456d686f62488c63b7713ef3e203f1b1
DNA5769 <dennisthomas2002@gmail.com>
2022-10-18 01:04:07 +0530

    Added rotation to texts

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,9 +3,13 @@ import { useState, useEffect } from "react";
 
 export default function Meme() {
 	const api_url = "https://api.imgflip.com/get_memes";
+	const R2D = 180 / Math.PI;
 	//this is to store all the meme's url returned by the api
 	const [allMemes, setAllMemes] = useState([]);
 	const [textInput, setTextInput] = useState("");
+	const [currRot, setCurrRot] = useState(null);
+	const [rotStartAngle, setRotStartAngle] = useState(0);
+	const [currRotAngle, setCurrRotAngle] = useState(0);
 
 	//this function run only once on component load
 	//when this component is mounted on page
@@ -46,14 +50,13 @@ export default function Meme() {
 
 	//this is to handle input change
 	function handleChange(event) {
-		 
 		const { value } = event.target;
 
 		setTextInput(value);
 	}
 	function handleTextSubmit(event) {
 		event.preventDefault();
-		if(textInput.length === 0){
+		if (textInput.length === 0) {
 			return;
 		}
 		//set the texts of the meme
@@ -62,9 +65,11 @@ export default function Meme() {
 			text: textInput,
 			style: {
 				left: "",
-				top: `${temp.length*30}px`,
-			}
-		})		
+				top: `${temp.length * 30}px`,
+				webkitTransform: "rotate(0deg)",
+			},
+			currAngle: 0,
+		});
 		setMeme((prev) => ({
 			...prev,
 			texts: temp,
@@ -110,7 +115,7 @@ export default function Meme() {
 					key={i}
 					id={i}
 					draggable
-					onDragStart={(e)=>handleDragStart(e, i)} 
+					onDragStart={(e) => handleDragStart(e, i)}
 					style={t.style}
 				>
 					{t.text}
@@ -120,24 +125,33 @@ export default function Meme() {
 					>
 						&#x2715;
 					</div>
+					<div
+						className="meme__text__rotate"
+						onMouseDown={(e) => startRot(e, i)}
+					>
+						&nbsp;
+					</div>
 				</h2>
 			)
-	)
-	
+	);
+
 	//these will be used to calculate the position of the texts while dragging
-	var p1 = 0, p2 = 0, p3 = 0, p4 = 0;
-	
-	//when the text starts to drag 
+	var p1 = 0,
+		p2 = 0,
+		p3 = 0,
+		p4 = 0;
+
+	//when the text starts to drag
 	const handleDragStart = (e, id) => {
 		p3 = e.clientX; //left and right distance of the cursor when we start dragging
 		p4 = e.clientY;
-		e.dataTransfer.setData("id",id);
-	}
+		e.dataTransfer.setData("id", id);
+	};
 
 	//when the text is dragged over the meme image
 	const handleDragOver = (e) => {
 		e.preventDefault();
-	}
+	};
 
 	//when the text is dropped on the meme image
 	const handleDrop = (e) => {
@@ -152,18 +166,81 @@ export default function Meme() {
 		const tempTexts = meme.texts;
 		//change the style of selected text and position it on the cursor
 		tempTexts[id].style = {
-			//distance of the dropped text should be equal to the difference between cursor's position 
+			...tempTexts[id].style,
+			//distance of the dropped text should be equal to the difference between cursor's position
 			//when it started drag and when it dropped. and subtract the offset of the element from it.
 			left: `${document.getElementById(String(id)).offsetLeft - p1}px`,
 			//similarly calculate top.
 			top: `${document.getElementById(id).offsetTop - p2}px`,
-		}
+		};
 		//set the temp texts to real texts in the meme state
-		setMeme((prev)=>({
+		setMeme((prev) => ({
 			...prev,
-			texts: tempTexts
-		}))
-	}
+			texts: tempTexts,
+		}));
+	};
+
+	const startRot = (e, id) => {
+		e.preventDefault();
+
+		setCurrRot(id);
+
+		const currRotEl = document.getElementById(id);
+
+		const bb = currRotEl.getBoundingClientRect(),
+			t = bb.top,
+			l = bb.left,
+			h = bb.height,
+			w = bb.width;
+
+		const centerX = l + w / 2,
+			centerY = t + h / 2;
+		setRotStartAngle(R2D * Math.atan2(e.clientY - centerY, e.clientX - centerX));
+	};
+
+	const textRot = e => {
+		if (currRot != null)
+		{
+			e.preventDefault();
+
+			const currRotEl = document.getElementById(currRot);
+
+			const bb = currRotEl.getBoundingClientRect(),
+				t = bb.top,
+				l = bb.left,
+				h = bb.height,
+				w = bb.width;
+
+			const centerX = l + w / 2,
+				centerY = t + h / 2;
+
+			const rotation = R2D * Math.atan2(e.clientY - centerY, e.clientX - centerX) - rotStartAngle;
+			setCurrRotAngle(rotation);
+
+			const tempTexts = meme.texts;
+			tempTexts[currRot].style = {
+				...tempTexts[currRot].style,
+				webkitTransform: `rotate(${tempTexts[currRot].currAngle + rotation}deg)`
+			};
+			setMeme((prev) => ({
+				...prev,
+				texts: tempTexts,
+			}));
+		}
+	};
+
+	const stopRot = (e) => {
+		e.preventDefault();
+
+		const tempTexts = meme.texts;
+		tempTexts[currRot].currAngle += currRotAngle;
+		setMeme((prev) => ({
+			...prev,
+			texts: tempTexts,
+		}));
+
+		setCurrRot(null);
+	};
 
 	return (
 		<div className="container">
@@ -200,12 +277,14 @@ export default function Meme() {
 			</form>
 			<div className="meme">
 				{meme.url && (
-					<img 
-						className="meme__image" 
-						src={meme.url} 
-						alt="meme" 
-						onDragOver={(e)=>handleDragOver(e)}
+					<img
+						className="meme__image"
+						src={meme.url}
+						alt="meme"
+						onDragOver={(e) => handleDragOver(e)}
 						onDrop={(e) => handleDrop(e)}
+						onMouseMove={e => textRot(e)}
+						onMouseUp={e => stopRot(e)}
 					/>
 				)}
 				{memeTexts}
